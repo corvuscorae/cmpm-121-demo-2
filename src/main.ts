4f98c733b95df9929f6b2ee80cba92a400296241
corvuscorae <corvuscorae@gmail.com>
2024-10-15 22:14:59 -0700

    Step 1: Initial non-interactive UI layout

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,13 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+// app title
+const title = document.createElement("h1");
+title.innerHTML = APP_NAME;
+app.append(title);
+
+// canvas
+const canvas = document.createElement("canvas");
+canvas.width = canvas.height = 256;
+app.append(canvas);
