87929fb439b817ab42ac12eb321cd858e9f86c5e
corvuscorae <corvuscorae@gmail.com>
2024-10-16 22:06:53 -0700

    Step 3: Display list and observer

--- src/main.ts
+++ src/main.ts
@@ -22,33 +22,59 @@ ctx?.fillRect(0, 0, myCanvas.width, myCanvas.height);
 const cursor = { active: false, x: 0, y: 0 };
 
 // draw on canvas
+let lines: { x: number; y: number; }[][] = [];
+let currentLine: { x: number; y: number; }[] = [];
+
+const drawingChanged = new Event("drawing-changed");
+
 myCanvas.addEventListener("mousedown", (e) => {
     cursor.active = true;
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+
+    currentLine = [];
+    lines.push(currentLine);
+    currentLine.push({x: cursor.x, y: cursor.y});
 });
 
 myCanvas.addEventListener("mouseup", () => {
     cursor.active = false;
+    currentLine = [];
 });
 
 myCanvas.addEventListener("mousemove", (e) => {
     if (cursor.active) {
-      ctx?.beginPath();
-      ctx?.moveTo(cursor.x, cursor.y);
-      ctx?.lineTo(e.offsetX, e.offsetY);
-      ctx?.stroke();
       cursor.x = e.offsetX;
       cursor.y = e.offsetY;
+      currentLine.push({x: cursor.x, y: cursor.y});
+
+      myCanvas.dispatchEvent(drawingChanged);
+    }
+});
+
+myCanvas.addEventListener("drawing-changed", (e)=>{
+    ctx?.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    ctx?.fillRect(0, 0, myCanvas.width, myCanvas.height);
+    for (const line of lines) {
+        if (line.length > 1) {
+            ctx?.beginPath();
+            const { x, y } = line[0];
+            ctx?.moveTo(x, y);
+            for (const { x, y } of line) {
+                ctx?.lineTo(x, y);
+            }
+            ctx?.stroke();
+        }
     }
 });
 
 // clear canvas
 const clearButton = document.createElement("button");
 clearButton.innerHTML = "clear";
-app.append(clearButton);
+app.append(document.createElement("div"), clearButton);
 
 clearButton.addEventListener("click", () => {
+    lines = [];
     ctx?.clearRect(0, 0, myCanvas.width, myCanvas.height);
     ctx?.fillRect(0, 0, myCanvas.width, myCanvas.height);
 });
