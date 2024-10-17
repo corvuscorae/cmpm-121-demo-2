2bb7fef2893c4414513509077e0bde266632c353
corvuscorae <corvuscorae@gmail.com>
2024-10-16 21:31:33 -0700

    Step 2: Simple marker drawing

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,44 @@ title.innerHTML = APP_NAME;
 app.append(title);
 
 // canvas
-const canvas = document.createElement("canvas");
-canvas.width = canvas.height = 256;
-app.append(canvas);
+const myCanvas = document.createElement("canvas");
+myCanvas.width = myCanvas.height = 256;
+app.append(myCanvas);
+
+const ctx = myCanvas.getContext("2d");
+ctx.fillStyle = "white";
+ctx?.fillRect(0, 0, myCanvas.width, myCanvas.height);
+
+const cursor = { active: false, x: 0, y: 0 };
+
+// draw on canvas
+myCanvas.addEventListener("mousedown", (e) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+});
+
+myCanvas.addEventListener("mouseup", () => {
+    cursor.active = false;
+});
+
+myCanvas.addEventListener("mousemove", (e) => {
+    if (cursor.active) {
+      ctx?.beginPath();
+      ctx?.moveTo(cursor.x, cursor.y);
+      ctx?.lineTo(e.offsetX, e.offsetY);
+      ctx?.stroke();
+      cursor.x = e.offsetX;
+      cursor.y = e.offsetY;
+    }
+});
+
+// clear canvas
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+app.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+    ctx?.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    ctx?.fillRect(0, 0, myCanvas.width, myCanvas.height);
+});
